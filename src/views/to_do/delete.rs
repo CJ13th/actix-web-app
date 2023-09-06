99301bd4141ec7eabd6f7697014e417703d81b4f
Connor Campbell <ccampbell.james@gmail.com>
2023-09-06 13:09:20 +0200

    refactor delete + edit endpoint

--- src/views/to_do/delete.rs
+++ src/views/to_do/delete.rs
@@ -1,25 +1,38 @@
+use crate::database::establish_connection;
+use crate::diesel;
+use diesel::prelude::*;
+
 use actix_web::{web, HttpResponse};
-use serde_json::value::Value;
-use serde_json::Map;
 
 use crate::json_serialization::{to_do_item::ToDoItem, to_do_items::ToDoItems};
 use crate::jwt::JwToken;
-use crate::processes::process_input;
-use crate::state::read_file;
-use crate::to_do::{enums::TaskStatus, to_do_factory};
+
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
+
+// pub async fn delete(to_do_item: web::Json<ToDoItem>, token: JwToken) -> HttpResponse {
+//     let state: Map<String, Value> = read_file("./state.json");
+//     let status: TaskStatus;
+
+//     match &state.get(&to_do_item.title) {
+//         Some(item) => status = TaskStatus::from_string(item.as_str().unwrap().to_string()),
+//         None => {
+//             return HttpResponse::NotFound().json(format!("{} not in state", to_do_item.title));
+//         }
+//     }
+
+//     let existing_item = to_do_factory(to_do_item.title.as_str(), status.clone());
+//     process_input(existing_item, "delete".to_owned(), &state);
+//     return HttpResponse::Ok().json(ToDoItems::get_state());
+// }
 
 pub async fn delete(to_do_item: web::Json<ToDoItem>, token: JwToken) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json");
-    let status: TaskStatus;
+    let connection = establish_connection();
 
-    match &state.get(&to_do_item.title) {
-        Some(item) => status = TaskStatus::from_string(item.as_str().unwrap().to_string()),
-        None => {
-            return HttpResponse::NotFound().json(format!("{} not in state", to_do_item.title));
-        }
-    }
+    let results = to_do::table.filter(to_do::columns::title.eq(&to_do_item.title));
 
-    let existing_item = to_do_factory(to_do_item.title.as_str(), status.clone());
-    process_input(existing_item, "delete".to_owned(), &state);
+    let _ = diesel::delete(results).execute(&connection);
+
     return HttpResponse::Ok().json(ToDoItems::get_state());
 }
