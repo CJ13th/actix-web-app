e770cb21a9ec0f1bed6518180c25bbc8b86e7b57
Connor Campbell <ccampbell.james@gmail.com>
2023-09-06 11:55:46 +0200

    make create endpoint use diesel db

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,10 +1,11 @@
+use crate::diesel;
 use actix_web::HttpRequest;
-use serde_json::value::Value;
-use serde_json::Map;
+use diesel::prelude::*;
 
-use crate::processes::process_input;
-use crate::state::read_file;
-use crate::to_do::{enums::TaskStatus, to_do_factory};
+use crate::database::establish_connection;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
 
 use crate::json_serialization::to_do_items::ToDoItems;
 use actix_web::HttpResponse;
@@ -16,10 +17,28 @@ use actix_web::HttpResponse;
 //     process_input(item, "create".to_string(), &state);
 //     return format!("{} created", title);
 // }
+// pub async fn create(req: HttpRequest) -> HttpResponse {
+//     let state: Map<String, Value> = read_file("./state.json");
+//     let title: String = req.match_info().get("title").unwrap().to_string();
+//     let item = to_do_factory(&title.as_str(), TaskStatus::PENDING);
+//     process_input(item, "create".to_string(), &state);
+//     return HttpResponse::Ok().json(ToDoItems::get_state());
+// }
+
 pub async fn create(req: HttpRequest) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json");
     let title: String = req.match_info().get("title").unwrap().to_string();
-    let item = to_do_factory(&title.as_str(), TaskStatus::PENDING);
-    process_input(item, "create".to_string(), &state);
+    let connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(&title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
+
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&connection);
+    }
     return HttpResponse::Ok().json(ToDoItems::get_state());
 }
